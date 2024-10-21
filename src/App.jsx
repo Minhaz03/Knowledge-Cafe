8546b613b11b51b1b36d70b2efa224569d6ea8d8
Minhaz03 <minhazulalamsohan2002@gmail.com>
2024-10-21 20:44:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl bg-red-400 text-center'>Knowledge Cafe</h1>
+      
+    </>
+  )
+}
+
+export default App
